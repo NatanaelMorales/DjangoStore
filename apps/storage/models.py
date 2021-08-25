47f1af2556112c4bc917fe6cb2b589b27e43b9f6
RodnyMorales <rodnymorales739@gmail.com>
2021-08-25 16:14:08 -0600

    actualización tabla

--- apps/storage/models.py
+++ apps/storage/models.py
@@ -15,18 +15,20 @@ class seller(models.Model): #vendedor
     name_seller = models.CharField(max_length=20, null=False, blank=False)
     telephone = models.CharField(max_length=8, null=False, blank=False)
 
-class order(models.Model): #envio
-    pk_num_order = models.AutoField(primary_key=True, null=False, blank=False)
-    fk_customer = models.ForeignKey(customer, null=False, blank=False, on_delete=models.CASCADE)
-    shipping_address = models.CharField(max_length=80, null=False, blank=False)
-    send_date = models.DateField(auto_now=False, auto_now_add=True, null=False, blank=False)
-    amount = models.IntegerField(null=False, blank=False)
-    fk_seller = models.OneToOneField(seller, null=False, blank=False, on_delete=models.CASCADE)
-
 class product(models.Model): #producto
     pk_product = models.AutoField(primary_key=True, null=False, blank=False)
     code = models.CharField(max_length=8, null=False, blank=False)
     name = models.CharField(max_length=40, null=False, blank=False)
     description = models.TextField(null=False, blank=False)
     price = models.DecimalField(null=False, blank=False, max_digits=3, decimal_places=2)
+
+class order(models.Model): #envio
+    pk_num_order = models.AutoField(primary_key=True, null=False, blank=False)
+    fk_customer = models.ForeignKey(customer, null=False, blank=False, on_delete=models.CASCADE)
+    shipping_address = models.CharField(max_length=80, null=False, blank=False)
+    send_date = models.DateField(auto_now=False, auto_now_add=True, null=False, blank=False)
+    amount = models.IntegerField(null=False, blank=False)
+    fk_product = models.OneToOneField(product, null=False, blank=False, on_delete=models.CASCADE)
+
+
 
