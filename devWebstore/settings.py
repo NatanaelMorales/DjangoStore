378353c27ba55159aeb8083b4acef2e126964da1
RodnyMorales <rodnymorales739@gmail.com>
2021-09-23 02:15:13 -0600

    hola

--- devWebstore/settings.py
+++ devWebstore/settings.py
@@ -83,11 +83,11 @@ DATABASES = {
 
     'default': {
        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'DevWebStore',
+        'NAME': 'DevWebStores',
         'USER': 'DevNatanael',
         'PASSWORD': 'morales3014',
         'HOST': '127.0.0.1',
-        'PORT': 5433,
+        'PORT': 5432,
     }
 }
 
